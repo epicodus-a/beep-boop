99999ba3fd3b92b321c6f46b4058805213c8cca6
Adrianacmy <adrianamullaney@gmail.com>
2018-03-30 11:20:26 -0700

    set up project skeleton

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+Program replaces 0 with 'beep!'
+// specs:
+// 	Numbers divisible by 3 are replaced with "I'm sorry, Dave. I'm afraid I can't do that."
+// 	Numbers that contain a 1 are replaced (all digits) with "Boop!"
+// - Numbers that contain a 0 are replaced (all digits) with "Beep!"
+
+//
