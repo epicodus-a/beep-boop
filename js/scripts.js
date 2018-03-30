3f0b24df0674f203ad1e03b00a33461dd24f7715
Adrianacmy <adrianamullaney@gmail.com>
2018-03-30 13:25:27 -0700

    add reverse order

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@
 
 // business logic
 const beepBoop = (number, name) => {
-	debugger;
+	// debugger;
 	if (number % 3 === 0){
 		return 'I\'m sorry, ' + name[0].toUpperCase()+ name.substring(1) +'. I\'m afraid I can\'t do that.';
 	}else if(number.toString().includes("1")){
@@ -28,13 +28,26 @@ $().ready(function(){
 		e.preventDefault();
 		let number = $("input:text").val();
 		let name = $("input:text[name=name]").val();
-		if (number && number >= 0){
+		let order = $("input:radio[name=order]:checked").val();
+		$(".output").append("<span class='text-capitalize font-weight-bold'>"+name+"</span><span> you entered:" + number + ". Fruits as belows:</span>");
+
+		if (number >= 0){
 			result = [];
-			for(let i = 0; i <= parseInt(number); i++){
-				result.push(beepBoop(i, name));
+			resultKey = [];
+			if(order === 1){
+				for(let i = 0; i <= parseInt(number); i++){
+					resultKey.push(i);
+					result.push(beepBoop(i, name));
+				}
+			}else{
+				for(let i = parseInt(number); i >= 0; i--){
+					resultKey.push(i);
+					result.push(beepBoop(i, name));
+				}
 			}
-			result.forEach(function(element){
-				let child = "<p class='lead'>" + element + "</p>";
+
+			result.forEach(function(element, index){
+				let child = "<p class='lead'>"+ resultKey[index] + ": " + element + "</p>";
 				$(".output").append(child);
 			});
 		}else{
